c45274fb5ff80782505d15430ce4eec3b19b598b
kazumi <kazumi121589@yahoo.co.jp>
2022-03-31 21:41:00 +0900

    ユーザー一覧画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
+  get 'users/index'
+  get 'users/show'
+  resources :users, only: [:index, :show] 
   resources :books
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'books#index'
 end
