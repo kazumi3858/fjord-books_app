783ee084ea6e37e01fae21a6b3b65f4df13de58d
kazumi <kazumi121589@yahoo.co.jp>
2022-04-08 15:12:05 +0900

    Modified user function

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
-  get 'users/index'
-  get 'users/show'
   resources :users, only: [:index, :show] 
   resources :books
   root 'books#index'
